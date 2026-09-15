b6e6b95516608a11db8de3e1c65286675b69b001
Aswinsujin7 <aswinsujin807@gmail.com>
2026-09-15 13:25:46 +0530

    Add initial HTML and JavaScript files for Employee Management System

---
+++ Employee Management System/script.js
@@ -0,0 +1,68 @@
+let employee = [
+    {
+        id: 101,
+        name: "Suresh",
+        age: 22,
+        department: "Development",
+        salary: 30000,
+        status: "Active"
+    },
+    {
+        id: 105,
+        name: "Ramesh",
+        age: 25,
+        department: "Testing",
+        salary: 35000,
+        status: "Active"
+    },
+    {
+        id:109,
+        name: "Mahesh",
+        age: 28,
+        department: "Development",
+        salary: 40000,
+        status: "Active"
+    },
+    {id:112,
+        name: "Nithin",
+        age: 30,
+        department: "Development",
+        salary: 60000,
+        status: "In Active"
+    },
+    {
+        id:115,
+        name: "gokul",
+        age: 31,
+        department: "Testing",
+        salary: 35000,
+        status: "In Active"
+    },
+    {
+        id:118,
+        name: "Rohit",
+        age: 32,  
+        department: "Development",
+        salary: 45000,
+        status: "Active"
+    },
+    {
+        id:120,
+        name: "Ananya",
+        age: 29,
+        department: "Marketing",
+        salary: 25000,
+        status: "Active"
+    },
+    {
+        id:122,
+        name: "Priya",
+        age: 27,
+        department: "Marketing",
+        salary: 30000,
+        status: "In Active"    
+    }
+
+];
+
+console.log(employee);
